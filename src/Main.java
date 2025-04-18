eaaa74a72a5edf85e585a481219db74f3330bd6f
Task View <maumauriciog@hotmail.com>
2025-04-18 10:52:51 -0300

    verificando se um numero é impar ou par

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int number = 0;
-
         if (number == 7){
             System.out.println("O número 7 é impar!");
         }else if (number == 8){
